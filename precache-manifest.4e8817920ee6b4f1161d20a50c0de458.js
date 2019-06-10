5c2c73f112390b3aebcd1cc80cd2ecde99db9c0d
Dowan Kim <dowankim@dyn-160-39-253-134.dyn.columbia.edu>
2019-06-10 19:31:50 -0400

    Updates

---
+++ precache-manifest.4e8817920ee6b4f1161d20a50c0de458.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d66517cb46270f28c35a51180e79640",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "5f12d3d9f4799bb4b4f5",
+    "url": "/clicky-game/static/css/main.32228b0e.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "5f12d3d9f4799bb4b4f5",
+    "url": "/clicky-game/static/js/main.e90e6b82.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
